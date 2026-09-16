3e6b5109dddc12ef311973085184765aa92aaef6
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-09-16 20:59:40 -0300

    Primer commit

---
+++ app/src/main/java/com/example/taller214/ui/theme/HomeScreen.kt
@@ -0,0 +1,56 @@
+package com.example.taller214.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.taller214.R
+import androidx.compose.foundation.layout.Arrangement
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(){
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {Text("Mi app Kotlin")})
+        }
+    ) {innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            Text(text="¡Bienvenido!")
+            Button(onClick={/* accion futura */}) {
+                Text("Presioname")
+            }
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
